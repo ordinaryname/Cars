346839b127d972e1e3bc62786b26010cdc6d4732
Raymond-MacbookPro <ordinaryapple@gmail.com>
2018-12-16 22:18:27 -0800

    Fixed issues w/ navigation on AWS server. Front page improvements.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Header';
 
+import bannerImage from './images/cars-proj-banner.jpg';
+
 class Home extends Component {
 
   render() {
@@ -8,11 +10,19 @@ class Home extends Component {
       <div>
         <Header/>
         <div className="imageContainer">
-          <img src={require('./images/cars-proj-banner.jpg')} className="bannerImage" alt="Chevy and Porsche Racecars"/>
+          <img src={bannerImage} className="bannerImage" alt="Chevy and Porsche Racecars"/>
           <div className="bannerText">
             <p className="bTextHead">This site was built with ReactJs, PHP & MySql</p>
             <p className="bText">This site was built with ReactJs, PHP & MySql and runs on AWS, Ubuntu, and Apache. It shows my skills at building a website, database, and api.</p>
           </div>
+        </div>
+        <div className="languagesContainer">
+          <ul className="listOLanguages">
+            <li className="language"><p className="languageText">React JS</p></li>
+            <li className="language"><p className="languageText">Javascript</p></li>
+            <li className="language"><p className="languageText">PHP</p></li>
+            <li className="language"><p className="languageText">MySql</p></li>
+          </ul>
         </div>
       </div>
     );
