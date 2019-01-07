571889525e7bb8bf5d0c75f1b8ce5b66c32be22d
Raymond-MacbookPro <ordinaryapple@gmail.com>
2019-01-06 18:12:01 -0800

    first commit of 2019

---
+++ src/Components/Account.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+import bannerImage from './images/cars-proj-banner.jpg';
+
+class Account extends Component {
+
+
+}
+
+export default Account;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Manufacturers from './Manufacturers';
 import { Link } from "react-router-dom";
 
+import accountIcon from './images/account_box.png';
+
 class Header extends Component {
   constructor() {
     super();
-    this.state = {showMenu:false, reference:"", query:"undefined", manufacturers:[], manufacturersList:[],};
+    this.state = {showMenu:false, showAccountMenu:false, reference:"", query:"undefined", manufacturers:[], manufacturersList:[],};
+    this.showAccountMenu = this.showAccountMenu.bind(this);
+    this.closeAccountMenu = this.closeAccountMenu.bind(this);
     this.showMenu = this.showMenu.bind(this);
     this.closeMenu = this.closeMenu.bind(this);
     this.updateSearch = this.updateSearch.bind(this);
@@ -25,6 +29,7 @@ class Header extends Component {
 
   updateSearch(event) {
     this.setState({reference:event.target.value});
+    this.setState({showMenu:true}, ()=> {document.addEventListener('click', this.closeMenu);});
   }
 
   searchFunction(event) {
@@ -32,10 +37,23 @@ class Header extends Component {
     var m = [];
     for(var i = 0; i < this.state.manufacturersList.length; i++) {
       if(this.state.manufacturersList[i].toLowerCase().indexOf(this.state.reference.toLowerCase()) > -1) {
-        m[m.length] = (<button className="manufacturer">{this.state.manufacturersList[i]}</button>);
+        m[m.length] = (<Link to={("/" + this.state.manufacturersList[i])}><button className="manufacturer">{this.state.manufacturersList[i]}</button></Link>);
       }
     }
     this.setState({manufacturers:m});
+  }
+
+  showAccountMenu(event) {
+    event.preventDefault();
+    this.setState({showAccountMenu:true}, ()=> {document.addEventListener('click', this.closeAccountMenu);});
+  }
+
+  closeAccountMenu(event) {
+    try {
+      if (!this.dropdownMenu1.contains(event.target)) {
+        this.setState({showAccountMenu:false}, ()=> {document.removeEventListener('click', this.closeAccountMenu);});
+      }
+    } catch(err) {}
   }
 
   showMenu(event) {
@@ -57,12 +75,17 @@ class Header extends Component {
         <a href="https://raymondmutyaba.com/" className="homeButton">RM</a>
         <ul className="listMenu">
           <li className="menuItem">
-            <input type="text" className="search" placeholder="Search for a car ..." value={this.state.reference} onChange={this.updateSearch} onKeyUp={this.searchFunction}/>
-            <div className="menu1" ref={(element) => {this.dropdownMenu = element;}}><div className="menu">{this.state.manufacturers}</div></div>
+            <img src={accountIcon} className="accountIcon" alt="Account Icon" onClick={this.showAccountMenu}/>
+            {this.state.showAccountMenu?(
+              <div className="accountMenu" ref={(element) => {this.dropdownMenu1 = element;}}>
+                <button className="manufacturer">Account</button>
+                <button className="manufacturer">Log Out</button>
+              </div>
+            ):(null)}
           </li>
           <li className="menuItem">
-            <button className="showManufacturers" onClick={this.showMenu}>Manufacturers</button>
-            {this.state.showMenu?(<div className="menu1" ref={(element) => {this.dropdownMenu = element;}}><Manufacturers/></div>):(null)}
+            <input type="text" className="search" placeholder="Search for a car ..." value={this.state.reference} onChange={this.updateSearch} onKeyUp={this.searchFunction}/>
+            {this.state.showMenu?(<div className="menu1" ref={(element) => {this.dropdownMenu = element;}}>{this.state.manufacturers}</div>):(null)}
           </li>
           <Link to="/"><li className="menuItem"><button className="showManufacturers">Cars Project</button></li></Link>
         </ul>
